4c63b8560b2f9a1b726404f9c8288ea3e727fa19
unknown <emprenderusinque@hotmail.com>
2022-08-09 15:40:42 -0500

    reload

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -8,8 +8,8 @@ export const AppRouter = () => {
     return (
         <BrowserRouter>
             <h1 className='title'>ComicBook</h1>
-            <Routes  basename={process.env.PUBLIC_URL} >
-                <Route path='/' element={<IssuesScreen />} exact/>
+            <Routes  >
+                <Route path={process.env.PUBLIC_URL + '/'} element={<IssuesScreen />} exact/>
                 <Route path='issue/:issueId' element={<IssueScreen />} />
             </Routes>
         </BrowserRouter>
