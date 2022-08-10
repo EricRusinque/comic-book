2b333e130ac163f3387f30538e4210e683d8f95e
unknown <emprenderusinque@hotmail.com>
2022-08-09 20:03:58 -0500

    reload

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,4 @@
 import { HashRouter, Route, Routes } from 'react-router-dom';
-import { GridList } from '../components/home-comic';
 import { IssuesScreen } from '../components/home-comic/IssuesScreen';
 import { IssueScreen } from '../components/issue-screen/IssueScreen';
 import './app-router.css'
@@ -8,9 +7,9 @@ export const AppRouter = () => {
     return (
         <HashRouter>
             <h1 className='title'>ComicBook</h1>
-            <Routes  >
-                <Route path='comic-book/' element={<IssuesScreen />}/>
-                <Route path='comic-book/issue/:issueId' element={<IssueScreen />} />
+            <Routes>
+                <Route path='/' element={<IssuesScreen />}/>
+                <Route path='issue/:issueId' element={<IssueScreen />} />
             </Routes>
         </HashRouter>
     );
